a543ce84d349421b6e2683e9f80df0fc19d1633a
xyz <xyz@example.com>
2023-03-09 22:44:10 +0530

    09-03-2023

---
+++ loop2/src/loop2/Sample.java
@@ -0,0 +1,21 @@
+package loop2;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		for(;i<=5;i++)
+		{
+			System.out.println("I value is::"+i);
+		}
+		int k=0;
+		for(;k<=5;)
+		{
+			k++;
+			System.out.println("K value is::"+k);
+		}
+
+	}
+
+}
---
+++ loop3/src/loop3/Sample.java
@@ -0,0 +1,23 @@
+package loop3;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i,n=5;
+		i=0;
+		while(i<=n)
+		{
+			System.out.println("I value is::"+i);
+			i++;
+		}
+		int k=0,m=5;
+		do
+		{
+			System.out.println("K value is::"+k);
+			k++;
+		}while(k<=m);
+
+	}
+
+}
